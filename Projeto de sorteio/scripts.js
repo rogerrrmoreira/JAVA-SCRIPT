d5198d209a96474f99583627076bdf7229bc2712
rogerrrmoreira <rogerrrmoreira@gmail.com>
2026-03-01 19:03:45 -0300

    projeto sorteio, acresentei um input para mostrar o resultado do sorteio, e adicionei um id ao botão de sortear para facilitar a manipulação via JavaScript. Também corrigi o link para o arquivo CSS e adicionei a tag script para conectar o arquivo JavaScript.

--- Projeto de sorteio/scripts.js
+++ Projeto de sorteio/scripts.js
@@ -2,5 +2,28 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
     const resul = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert(resul);
-}
+    const btnSortear = document.querySelector('#btnSortear');
+    const resultadoOutput = document.querySelector('.result');
+
+  }
+
+  const resultadoOutput = document.querySelector('.result');
+
+  btnSortear.addEventListener('click', () => {
+    
+     const min = Math.ceil(document.querySelector('.input-min').value);
+    const max = Math.floor(document.querySelector('.input-max').value);
+    
+    if (isNaN(min) || isNaN(max)) {
+      resultadoOutput.value = 'Por favor, insira números válidos.';
+      return;
+    }
+
+    if (min > max) {
+      resultadoOutput.value = 'O mínimo deve ser menor ou igual ao máximo.';
+      return;
+    }
+
+    const numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+    resultadoOutput.value = numeroAleatorio;
+  });
