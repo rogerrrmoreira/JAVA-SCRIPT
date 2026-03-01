74672a36c58d75b86daf8268ad25b34cf92ee682
rogerrrmoreira <rogerrrmoreira@gmail.com>
2026-03-01 19:25:18 -0300

    at 2024-06-01T12:00:00Z

--- Projeto de sorteio/scripts.js
+++ Projeto de sorteio/scripts.js
@@ -13,7 +13,7 @@ function generateNumber() {
     
      const min = Math.ceil(document.querySelector('.input-min').value);
     const max = Math.floor(document.querySelector('.input-max').value);
-    
+
     if (isNaN(min) || isNaN(max)) {
       resultadoOutput.value = 'Por favor, insira números válidos.';
       return;
@@ -26,4 +26,24 @@ function generateNumber() {
 
     const numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
     resultadoOutput.value = numeroAleatorio;
+    dataAtualP.textContent = `Data do sorteio: ${getDataFormatada()}`;
   });
+
+  //////FUNÇAO PARA DARA //////
+
+  const zeroEsquerda = num => (num < 10 ? '0' + num : num);
+
+function getDataFormatada() {
+  const agora = new Date();
+
+  const dia = zeroEsquerda(agora.getDate());
+  const mes = zeroEsquerda(agora.getMonth() + 1);
+  const ano = agora.getFullYear();
+  const hora = zeroEsquerda(agora.getHours());
+  const minutos = zeroEsquerda(agora.getMinutes());
+  const segundos = zeroEsquerda(agora.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${minutos}:${segundos}`;
+}
+
+const dataAtualP = document.querySelector('.data-atual');
